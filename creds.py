8a44d06fb4cff9e7ec24dccf3917d7678bf6fda5
Genbogo <66338822+genbogo@users.noreply.github.com>
2021-04-26 13:26:03 +0000

    Update creds.py

--- creds.py
+++ creds.py
@@ -1,6 +1,6 @@
 class Creds():
     # ENTER Your bot Token Here
-    TG_TOKEN = ""
+    TG_TOKEN = "1736951147:AAFLfkRY79qkkVzh8VjOlRTnz-G7ILghCsM"
     
     
     
@@ -8,10 +8,10 @@ class Creds():
     # Because of pydrive And pydrive v2 Api
     
     #Folder Id Of Teamdrive
-    TEAMDRIVE_FOLDER_ID = ""
+    TEAMDRIVE_FOLDER_ID = "1rsC9vBeSHOCPQE6AX_2R0lVunCTxBqx4"
     
     # Id of Team drive 
-    TEAMDRIVE_ID = ""
+    TEAMDRIVE_ID = "0AD8tQfmXodTNUk9PVA"
     
     
     
